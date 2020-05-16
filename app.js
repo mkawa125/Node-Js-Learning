6b95584d49119e4123131467fb0c83e8a0b53c47
dahabu saidi <dahabusaidi@gmail.com>
2020-05-16 22:02:07 +0300

    Creating custom sever

--- app.js
+++ app.js
@@ -3,10 +3,17 @@
  * Extracting some function from helper class
  * */
 
-// const helpers = require('./helpers')
-
 const { sum } = require('./helpers')
 
+// Core module comes with node.js
+const http = require('http')
+
+const server = http.createServer((req , res) =>
+{
+    res.end('Hello from mkawa')
+});
+
+server.listen(3000);
 
 const total = sum(5, 6);
 console.log(total);
