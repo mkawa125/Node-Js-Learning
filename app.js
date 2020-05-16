15620223b41e0693cf99f14c1e8b79ba345b5812
dahabu saidi <dahabusaidi@gmail.com>
2020-05-16 23:09:21 +0300

    Use sychronous functions

--- app.js
+++ app.js
@@ -30,12 +30,15 @@ const fs = require('fs');
 
 const fileName = "target.txt";
 
-fs.readFile(fileName, (err, data) =>
-{
-    if (err) {
-        console.log(err)
-    }
-    console.log(data.toString())
-});
+const data = fs.readFileSync(fileName)
+console.log(data.toString())
+
+// fs.readFile(fileName, (err, data) =>
+// {
+//     if (err) {
+//         console.log(err)
+//     }
+//     console.log(data.toString())
+// });
 
 console.log('Node js is asynchronous programming ... ?')
